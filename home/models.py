fa641e415e1c5881836a317185667e30631132d4
Stuart Axon <stuaxo2@yahoo.com>
2018-02-08 10:43:14 +0000

    Feedback from PR Add missing migration

--- home/models.py
+++ home/models.py
@@ -24,10 +24,12 @@ class HomePage(Page):
     ]
 
     def get_context(self, request):
+        # imports here, as otherwise a circular dependency can happen
+        # during the initial migration
         from industry.models import IndustryPage
         from setup_guide.models import SetupGuidePage
-        context = dict(
-            **super().get_context(request),
+        context = super().get_context(request)
+        context.update(
             industry_cards=IndustryPage.objects.live(),
             setup_guide_cards=SetupGuidePage.objects.live(),
         )
