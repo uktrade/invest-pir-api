46d961da2ccbd1ed00bb299712644c8c5b0dc32b
Stuart Axon <stuaxo2@yahoo.com>
2018-03-08 13:13:45 +0000

    Fix form post locations. Remove unused child menu logic. Footer -    add nav menu    add secondary nav    add copyright

--- invest/models.py
+++ invest/models.py
@@ -3,10 +3,15 @@ wagtail.contrib.settings - hold global text and images, e.g.
 for navbar, social buttons
 """
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 
 from wagtail.contrib.settings.models import BaseSetting
 from wagtail.contrib.settings.registry import register_setting
+from wagtail.core.blocks import PageChooserBlock, CharBlock, StructBlock, URLBlock
+from wagtail.core.fields import StreamField
 from wagtail.images.edit_handlers import ImageChooserPanel
+
+from invest.blocks.external_link import ExternalLinkBlock
 
 
 @register_setting
@@ -19,6 +24,25 @@ class Branding(BaseSetting):
         related_name='+'
     )
 
+    footer_logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    footer_secondary_nav = StreamField([
+        ('internal_link', PageChooserBlock()),
+        ('external_link', ExternalLinkBlock()),
+         ],
+        blank=True)
+
+    footer_copyright = models.CharField(max_length=255)
+
     panels = [
         ImageChooserPanel('logo'),
+        ImageChooserPanel('footer_logo'),
+        StreamFieldPanel("footer_secondary_nav"),
+        FieldPanel('footer_copyright'),
     ]
--- invest/templatetags/navigation_tags.py
+++ invest/templatetags/navigation_tags.py
@@ -16,28 +16,12 @@ def get_site_root(context):
     return context['request'].site.root_page
 
 
-def has_menu_children(page):
-    # This is used by the top_menu property
-    # get_children is a Treebeard API thing
-    # https://tabo.pe/projects/django-treebeard/docs/4.0.1/api.html
-    return page.get_children().live().in_menu().exists()
-
-
-def has_children(page):
-    # Generically allow index pages to list their children
-    return page.get_children().live().exists()
-
-
 def is_active(page, current_page):
     # To give us active state on main navigation
-    return (current_page.url.startswith(page.url) if current_page else False)
+    return current_page.url.startswith(page.url) and current_page
 
 
-# Retrieves the top menu items - the immediate children of the parent page
-# The has_menu_children method is necessary because the Foundation menu
-# requires a dropdown class to be applied to a parent
-@register.inclusion_tag('tags/top_menu.html', takes_context=True)
-def top_menu(context, parent, calling_page=None):
+def get_menu_pages(context, parent, calling_page=None):
     root = get_site_root(context)
     menuitems = parent.get_children().live().in_menu()
     if parent == root:
@@ -47,7 +31,6 @@ def top_menu(context, parent, calling_page=None):
         if menuitem == root:
             # Treat root menuitem like it is on the next level
             # must match urls exactly as all URLs will start with homepage URL
-            menuitem.show_dropdown = False
             menuitem.active = (
                 (calling_page.url == menuitem.url)
                 if calling_page else False
@@ -56,33 +39,18 @@ def top_menu(context, parent, calling_page=None):
             # We don't directly check if calling_page is None since the
             # template engine can pass an empty string to calling_page
             # if the variable passed as calling_page does not exist.
-            menuitem.show_dropdown = has_menu_children(menuitem)
             menuitem.active = ((calling_page.url.startswith(menuitem.url)
                                if calling_page else False))
+    return menuitems
+
+
+@register.inclusion_tag('tags/top_menu.html', takes_context=True)
+def nav_menu(context, parent, calling_page=None, item_class="nav-item"):
+    menuitems = get_menu_pages(context, parent, calling_page)
     return {
         'calling_page': calling_page,
         'menuitems': menuitems,
-        # required by the pageurl tag that we want to use within this template
-        'request': context['request'],
-    }
-
-
-# Retrieves the children of the top menu items for the drop downs
-@register.inclusion_tag('tags/top_menu_children.html', takes_context=True)
-def top_menu_children(context, parent, calling_page=None):
-    menuitems_children = parent.get_children()
-    menuitems_children = menuitems_children.live().in_menu()
-    for menuitem in menuitems_children:
-        menuitem.has_dropdown = has_menu_children(menuitem)
-        # We don't directly check if calling_page is None since the template
-        # engine can pass an empty string to calling_page
-        # if the variable passed as calling_page does not exist.
-        menuitem.active = (calling_page.url.startswith(menuitem.url)
-                           if calling_page else False)
-        menuitem.children = menuitem.get_children().live().in_menu()
-    return {
-        'parent': parent,
-        'menuitems_children': menuitems_children,
+        'item_class': item_class,
         # required by the pageurl tag that we want to use within this template
         'request': context['request'],
     }
