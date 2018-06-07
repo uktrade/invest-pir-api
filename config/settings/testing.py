5847e2551b2c284e8564e5da179f5426728468db
Rollo Konig-Brock <rollokb@gmail.com>
2018-06-07 15:46:41 +0100

    Add test settings

---
+++ config/settings/testing.py
@@ -0,0 +1,10 @@
+from config.settings import *
+
+CELERY_ALWAYS_EAGER = True
+BROKER_BACKEND = 'memory'
+CELERY_ALWAYS_EAGER = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+REST_FRAMEWORK = {}
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
