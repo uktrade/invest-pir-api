47c0fd577e1ab0b8296c0bae2416189d106cc0fd
Rollo Konig-Brock <rollokb@gmail.com>
2018-06-07 14:34:03 +0100

    use context manager for language

--- investment_report/views/utils.py
+++ investment_report/views/utils.py
@@ -12,38 +12,39 @@ from investment_report.models import Market, Sector
 
 
 def investment_report_html(request, lang, market, sector, moderated=True):
+    """
+    Used for previewing on dev
+    """
     market = get_object_or_404(Market, name=market)
     sector = get_object_or_404(Sector, name=sector)
-
-    lang = request.GET.get('lang')
-    if lang:
-        translation.activate(lang)
-
     company = request.GET.get('company', 'You')
+    lang = request.GET.get('lang', settings.LANGUAGE_CODE)
 
-    return HttpResponse(
-        str(investment_report_html_generator(
-            market, sector, company, local=False, moderated=moderated
-        )[0])
-    )
+    with translation.override(lang):
+        return HttpResponse(
+            str(investment_report_html_generator(
+                market, sector, company, local=False, moderated=moderated
+            )[0])
+        )
 
 
 def investment_report_pdf(request, lang, market, sector, moderated=True):
-    response = HttpResponse(content_type='application/pdf')
-
+    """
+    Used for previewing on the admin
+    """
     market = get_object_or_404(Market, name=market)
     sector = get_object_or_404(Sector, name=sector)
+    company = request.GET.get('company', 'You')
     translation.activate(lang)
 
-    company = request.GET.get('company', 'You')
+    with translation.override(lang):
+        pdf_file = investment_report_pdf_generator(
+            market, sector, company, local=True, moderated=moderated
+        )
 
-    pdf_file = investment_report_pdf_generator(
-        market, sector, company, local=True, moderated=moderated
-    )
+        pdf = pdf_file.getvalue()
+        pdf_file.close()
 
-    pdf = pdf_file.getvalue()
-    pdf_file.close()
-    response.write(pdf)
-
-    translation.activate(settings.LANGUAGE_CODE)
-    return response
+        response = HttpResponse(content_type='application/pdf')
+        response.write(pdf)
+        return response
