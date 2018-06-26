abdf09b7a0d9ead1c106b351ef8b4ab6e188c355
Rollo Konig-Brock <rollokb@gmail.com>
2018-06-26 16:12:42 +0100

    add test for custom markdown footnotes

--- investment_report/tests/test_markdown.py
+++ investment_report/tests/test_markdown.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from django.test import SimpleTestCase
 from investment_report.markdown import custom_markdown
 
@@ -30,3 +31,21 @@ class TestCase(SimpleTestCase):
             self.assertEquals(
                 res2, '<p><img alt="" src="/media/markdownx/test.png" /></p>'
             )
+
+    def test_custom_markdwon_footnotes(self):
+        result = custom_markdown(
+            (
+                'Text to cite[^1]'
+                'Text to cite[^2]'
+                '\n\n'
+                '[^1]: Cite'
+                '\n\n'
+                '[^2]: Cite'
+            ), local=False
+        )
+
+        soup = BeautifulSoup(result, 'html.parser')
+        # Assert a numbered list is inserted inside the div (list position
+        # didn't work to well for layout in this case)
+        self.assertTrue(soup.findAll('p')[1].text.startswith('1.'))
+        self.assertTrue(soup.findAll('p')[2].text.startswith('2.'))
