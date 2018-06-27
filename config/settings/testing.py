89c54bc4ddd162cd661b29b146a3f4ba252ecfa5
Rollo Konig-Brock <rollokb@gmail.com>
2018-06-27 13:23:31 +0100

    Update test settings to explicity import explicity used var

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -1,4 +1,7 @@
+import os
+
 from config.settings import *  # noqa
+from config.settings import PROJECT_ROOT
 
 CELERY_ALWAYS_EAGER = True
 BROKER_BACKEND = 'memory'
@@ -7,3 +10,8 @@ CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 
 REST_FRAMEWORK = {}
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = "/media/"
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
