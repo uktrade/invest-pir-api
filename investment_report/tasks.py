c5f5be273873a696f5652b5e114c8ad6898d5311
Rollo Konig-Brock <rollokb@gmail.com>
2018-07-11 13:26:44 +0100

    Pep8

--- investment_report/tasks.py
+++ investment_report/tasks.py
@@ -10,6 +10,10 @@ def create_pdf(pir_request_id):
 
 @shared_task
 def send_default_investment_email(pir_request_id):
-    from investment_report.utils import send_default_investment_email as send_email
+    from investment_report.models import PIRRequest
+    from investment_report.utils import (
+        send_default_investment_email as send_email
+    )
+
     instance = PIRRequest.objects.get(id=pir_request_id)
     send_email(instance)
