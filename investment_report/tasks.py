214b61a40dd1e990524c3e2b72f348cc6e626fb6
Rollo Konig-Brock <rollokb@gmail.com>
2018-07-11 12:11:34 +0100

    Other sector email

--- investment_report/tasks.py
+++ investment_report/tasks.py
@@ -6,3 +6,10 @@ def create_pdf(pir_request_id):
     from investment_report.models import PIRRequest
     instance = PIRRequest.objects.get(id=pir_request_id)
     instance.create_pdf()
+
+
+@shared_task
+def send_default_investment_email(pir_request_id):
+    from investment_report.utils import send_default_investment_email as send_email
+    instance = PIRRequest.objects.get(id=pir_request_id)
+    send_email(instance)
