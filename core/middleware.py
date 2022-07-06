7d22ce88d42c242e414b99cac05fdde2734d435d
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2022-07-06 14:04:11 +0100

    Added test and sorted some missing env variables for tests

--- core/middleware.py
+++ core/middleware.py
@@ -1,11 +1,10 @@
 import logging
+import sigauth.middleware
 
 from django.conf import settings
 from django.http import HttpResponse
 from django.urls import resolve
 from django.utils.deprecation import MiddlewareMixin
-from django.http import HttpResponse
-import sigauth.middleware
 
 logger = logging.getLogger(__name__)
 
@@ -31,13 +30,15 @@ def AdminIpRestrictionMiddleware(get_response):
 
     return middleware
 
-class AdminPermissionCheckMiddleware(MiddlewareMixin):
 
+class AdminPermissionCheckMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwarg):
         if request.user is not None:
             if request.resolver_match.namespace == 'admin' or request.path_info.startswith('/admin/login'):
                 if not request.user.is_staff and request.user.is_authenticated:
                     return HttpResponse('User not authorized for admin access', status=401)
+
+
 class SignatureCheckMiddleware(
     sigauth.middleware.SignatureCheckMiddlewareBase
 ):
